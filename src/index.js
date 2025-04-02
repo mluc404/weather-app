fbb73d4e23f4d2fff9a15472cb1ae1b5de582703
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-04-02 08:00:02 -0700

    feat: setup basic weather search functionality with API call
    - Successfully fetch weather data from API
    - Add search bar for user input
    - Process user text input for API query
    - Log location and weather condition to console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import "./styles.css";
 
-console.log("testing");
+// const key = "PUZ7W68BYF79J538R4JS82TNM";
+const input = document.querySelector("#loc");
+const searchBtn = document.querySelector("#searchBtn");
+
+const processInput = (input) => {
+  let finalInput = "London";
+  const inputArr = input.value.toLocaleLowerCase().trim().split(" ");
+  if (inputArr.length > 0) {
+    finalInput = inputArr.join("%20");
+  } else {
+    finalInput = input;
+  }
+  return finalInput;
+};
+
+searchBtn.addEventListener("click", () => {
+  const finalInput = processInput(input);
+  getWeather(finalInput);
+});
+
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const finalInput = processInput(input);
+    getWeather(finalInput);
+  }
+});
+
+const getWeather = (finalInput) => {
+  fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+      finalInput +
+      "?unitGroup=us&key=PUZ7W68BYF79J538R4JS82TNM&contentType=json",
+    { mod: "cors" }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      console.log(response.resolvedAddress);
+      console.log(response.description);
+    });
+};
