55b4a4c5ada9e18b3a6cfe3ce05f9d705dbb2c64
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-04-02 12:06:34 -0700

    feat: change bg image according to weather condition
    - Use dynamic import to import image matched with condition

--- src/index.js
+++ src/index.js
@@ -23,25 +23,31 @@ async function getWeather(finalInput) {
       "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
         finalInput +
         "?unitGroup=us&key=PUZ7W68BYF79J538R4JS82TNM&contentType=json",
-      { mod: "cors" }
+      { mode: "cors" }
     );
     console.log(response);
     const jsonResponse = await response.json();
     return jsonResponse;
   } catch (error) {
     console.log(error);
+    alert("Please in put a proper location");
   }
 }
 
 // Function to process the data from API
 const processWeather = (jsonResponse) => {
   console.log(jsonResponse);
-  const locName = jsonResponse.resolvedAddress;
-  const locTime = jsonResponse.currentConditions.datetime;
-  const locTemp = jsonResponse.currentConditions.temp;
-  const locCondition = jsonResponse.currentConditions.conditions;
-  const locDescription = jsonResponse.description;
-  return { locName, locTime, locTemp, locCondition, locDescription };
+  try {
+    const locName = jsonResponse.resolvedAddress;
+    const locTime = jsonResponse.currentConditions.datetime;
+    const locTemp = jsonResponse.currentConditions.temp;
+    const locCondition = jsonResponse.currentConditions.conditions;
+    const locDescription = jsonResponse.description;
+    return { locName, locTime, locTemp, locCondition, locDescription };
+  } catch (error) {
+    console.log(error);
+    alert("Alert 2");
+  }
 };
 
 let form = document.querySelector("form");
@@ -75,6 +81,8 @@ input.addEventListener("keypress", async (e) => {
   }
 });
 
+const allConditions = ["clear", "rain", "sun", "cloud", "snow"];
+const main = document.querySelector("main");
 // Function to display weather
 const displayWeather = (weatherObj) => {
   // const locName = document.querySelector(".locName");
@@ -87,5 +95,17 @@ const displayWeather = (weatherObj) => {
 
   allDivs.forEach((div) => {
     div.textContent = weatherObj[div.className];
+  });
+
+  console.log(weatherObj.locCondition);
+  allConditions.forEach(async (name) => {
+    const mainCondition = weatherObj.locCondition
+      .toLocaleLowerCase()
+      .split(",")[0];
+    if (mainCondition.includes(name)) {
+      const bg = await import(`./images/${name}.jpeg`);
+      console.log(bg);
+      main.style.backgroundImage = `url(${bg.default})`;
+    }
   });
 };
